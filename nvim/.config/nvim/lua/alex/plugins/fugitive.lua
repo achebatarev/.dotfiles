f56043988d7f727006b574d95c87c42a222e0651
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2024-02-23 20:55:18 -0500

    Update push and add pull

--- nvim/.config/nvim/lua/alex/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/alex/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gi", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gp", vim.cmd.Git("push"))
+        vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("push") end)
+        vim.keymap.set("n", "<leader>gP", function() vim.cmd.Git("pull") end)
     end
 }
