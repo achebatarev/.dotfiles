69c9acfcdfd6dbe1e88910d0048b52566db52c67
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2024-02-23 20:47:21 -0500

    Remap push

--- nvim/.config/nvim/lua/alex/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/alex/plugins/fugitive.lua
@@ -2,6 +2,6 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gi", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gp", ":Git push -u origin HEAD")
+        vim.keymap.set("n", "<leader>gp", vim.cmd.Git("push"))
     end
 }
